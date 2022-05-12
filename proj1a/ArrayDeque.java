c979ca6d1cfaa59fce534ac475b74522c6915e6a
Lie <571736989@qq.com>
2022-05-12 19:45:39 +0800

    finished proj1a

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,66 +1,96 @@
 
 public class ArrayDeque<T> {
-    private int frontSize;
-    private int backSize;
+    private int nextFirst;
+    private int nextLast;
     private T[] items;
+    private int size;
 
     public ArrayDeque() {
         items = (T[]) new Object[8];
-        frontSize = 0;
-        backSize = 0;
+        nextFirst = 3;
+        nextLast = 4;
+        size = 0;
+
     }
     /** in order to save time I decided to start my Array in the middle.
      *  hope it works.
-     *  it seems the rate of usage 0.25 was prepared for this means. */
+     *  it seems the rate of usage 0.25 was prepared for this means.
+     *  it's wrong. the implement will not use above means*/
+
 
     public void addFirst(T item) {
-        if (items.length / 2 == frontSize) {
+        if (size == items.length) {
             T[] a = (T[]) new Object[items.length * 2];
-            System.arraycopy(items, 0, a, frontSize, frontSize + backSize);
-            items = a;
 
+            System.arraycopy(items, nextFirst + 1, a, 0, items.length - nextFirst - 1);
+            System.arraycopy(items, 0, a, items.length - nextFirst - 1, nextFirst + 1);
+            items = a;
+            items[items.length - 1] = item;
+            nextFirst = items.length - 2;
+            nextLast = items.length / 2;
+        } else if (nextFirst == 0) {
+            items[nextFirst] = item;
+            nextFirst = items.length - 1;
+        } else {
+            items[nextFirst] = item;
+            nextFirst -= 1;
         }
-        frontSize += 1;
-        items[items.length / 2 - frontSize] = item;
+        size += 1;
 
     }
 
 
     public void addLast(T item) {
-        if (backSize == items.length / 2) {
+        if (size == items.length) {
             T[] a = (T[]) new Object[items.length * 2];
-            System.arraycopy(items, items.length / 2 - frontSize, a,
-                    items.length - frontSize, frontSize + backSize);
+
+            System.arraycopy(items, nextFirst + 1, a, 0, items.length - nextFirst - 1);
+            System.arraycopy(items, 0, a, items.length - nextFirst - 1, nextFirst + 1);
             items = a;
-
+            items[items.length / 2] = item;
+            nextFirst = items.length - 1;
+            nextLast = items.length / 2 + 1;
+        } else if (nextLast == items.length - 1) {
+            items[nextLast] = item;
+            nextLast = 0;
+        } else {
+            items[nextLast] = item;
+            nextLast += 1;
         }
-
-        items[backSize + items.length / 2] = item;
-        backSize += 1;
+        size += 1;
 
     }
 
     public boolean isEmpty() {
-        return frontSize + backSize == 0;
+        return size == 0;
 
     }
 
     public int size() {
-        return frontSize + backSize;
+        return size;
 
     }
 
     public void printDeque() {
-        for (int i = 0; i < (frontSize + backSize); i++) {
-            System.out.print(items[items.length - frontSize + i] + " ");
+        if (nextFirst < nextLast) {
+            for (int i = nextFirst + 1; i < nextLast; i++) {
+                System.out.print(items[i] + " ");
+            }
+        } else {
+            for (int i = nextFirst + 1; i < items.length; i++) {
+                System.out.print(items[items.length - nextFirst + i] + " ");
+            }
+            for (int i = 0; i < nextLast; i++) {
+                System.out.print(items[i] + " ");
+            }
         }
-
     }
     /** check if array needs to be resized. if true do it. */
     private void resize() {
-        if ((frontSize + backSize) < items.length / 4.0 && items.length >= 16) {
+        if (size < items.length / 4 && items.length >= 16) {
             T[] a = (T[]) new Object[items.length / 2];
-            System.arraycopy(items, items.length / 4, a, 0, items.length / 2);
+            System.arraycopy(items, nextFirst + 1, a, 0, items.length - nextFirst - 1);
+            System.arraycopy(items, 0, a, items.length - nextFirst - 1, nextFirst + 1);
             items = a;
         }
     }
@@ -68,39 +98,49 @@ public class ArrayDeque<T> {
     public T removeFirst() {
         if (isEmpty()) {
             return null;
-        } else if (size() == 1) {
-            T value = items[items.length / 2 - frontSize];
-            frontSize = 0;
-            backSize = 0;
+        } else {
+            T value;
+            if (nextFirst == items.length - 1) {
+                value = items[0];
+                nextFirst = 0;
+            } else {
+                value = items[nextFirst + 1];
+                nextFirst += 1;
+            }
             resize();
+            size -= 1;
             return value;
         }
-        T value = items[items.length / 2 - frontSize];
-        frontSize -= 1;
-        resize();
-        return value;
 
     }
 
     public T removeLast() {
         if (isEmpty()) {
             return null;
-        } else if (size() == 1) {
-            T value = items[items.length / 2 + backSize - 1];
+        } else {
+            T value;
+            if (nextLast == 0) {
+                value = items[items.length - 1];
+                nextLast = items.length - 1;
+            } else {
+                value = items[nextLast - 1];
+                nextLast = nextLast - 1;
+            }
             resize();
-            backSize = 0;
-            frontSize = 0;
+            size -= 1;
             return value;
         }
-        T value = items[items.length / 2 + backSize - 1];
-        backSize -= 1;
-        resize();
-        return value;
 
     }
 
     public T get(int index) {
-        return items[items.length / 2 - frontSize + index];
+        if (index >= size) {
+            return null;
+        }
+        if (nextFirst + index + 1 < items.length - 1) {
+            return items[nextFirst + index + 1];
+        }
+        return items[index - (items.length - nextFirst - 1)];
 
     }
 
