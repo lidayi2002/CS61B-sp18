9a6e7d48b43a4f97b8b8904b35faedca102118c1
Lie <571736989@qq.com>
2023-01-29 15:22:26 +0800

    finished1 hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,91 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private final boolean[][] grid;
 
+    private final int[] adRow = {-1, 0, 0, 1};
+
+    private final int[] adCol = {0, -1, 1, 0};
+
+
+    private final WeightedQuickUnionUF id;
+    private final int width;
+    private int openSite = 0;
+    // create N-by-N grid, with all sites initially blocked
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("N should be positive.");
+        }
+        width = N;
+        grid = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                grid[i][j] = false;
+            }
+        }
+
+        id = new WeightedQuickUnionUF(N * N + 2);
+
+    }
+
+    //calculate the site position in the id;
+    private int calPosition(int row, int col) {
+        return (width * row + col + 2);
+    }
+
+    private void openSite(int row, int col) {
+        int newRow;
+        int newCol;
+        for (int i = 0; i < 4; i++) {
+            newRow = row + adRow[i];
+            newCol = col + adCol[i];
+            if (0 <= newRow && newRow < width && 0 <= newCol
+                    && newCol < width && isOpen(newRow, newCol)) {
+                id.union(calPosition(row, col), calPosition(newRow, newCol));
+            }
+        }
+    }
+
+
+    // open the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        if (row < 0 || col < 0 || row >= width || col >= width) {
+            throw new IndexOutOfBoundsException("Please enter int between 0 and " + width + ".");
+
+        }
+        if (!isOpen(row, col)) {
+            grid[row][col] = true;
+            openSite++;
+            if (row == 0) {
+                id.union(0, calPosition(row, col));
+            }
+            if (row == width - 1) {
+                id.union(1, calPosition(row, col));
+            }
+            openSite(row, col);
+        }
+
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        if (row < 0 || col < 0 || row >= width || col >= width) {
+            throw new IndexOutOfBoundsException("Please enter int between 0 and " + width + ".");
+
+        }
+        return grid[row][col];
+    }
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        return id.connected(0, calPosition(row, col));
+    }
+    public int numberOfOpenSites() {
+        return openSite;
+    }          // number of open sites
+    public boolean percolates() {
+        return id.connected(0, 1);
+    }          // does the system percolate?
+    public static void main(String[] args) {
+
+    }   // use for unit testing (not required)
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,47 @@
 package hw2;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.introcs.StdRandom;
+
+
 
 public class PercolationStats {
+    private double[] threshold;
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("N or T must > 0. ");
+        }
 
+        threshold = new double[T];
+        for (int i = 0; i < T; i++) {
+            Percolation trial = pf.make(N);
+            while (!trial.percolates()) {
+                int row = StdRandom.uniform(0, N);
+                int col = StdRandom.uniform(0, N);
+                trial.open(row, col);
+
+            }
+            threshold[i] = trial.numberOfOpenSites() / (double) (N * N);
+
+
+
+        }
+
+    }
+
+    public double mean() {
+        return StdStats.mean(threshold);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(threshold);
+    }
+
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev();
+    }
+
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev();
+    }
 }
+
